c7dc900cf9ae504de73d54f7cac7469017148d47
TheoMangos <130418904+TheoMangos@users.noreply.github.com>
2024-04-26 10:45:55 +1000

    asdf

--- test/cvxcv/launch/cvxcv.launch.py
+++ test/cvxcv/launch/cvxcv.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='sdfqwsd000',
+            package='cvxcv',
             executable='Node1.py',
             name='node1'
         ),
